b503fec2ef2139adcf75f66d983022f6f382a1af
bertabenet <bertabenet@users.noreply.github.com>
2020-04-16 20:24:27 +0200

    intro to scripting

--- Assets/RW/Scripts/Managers/UIManager.cs
+++ Assets/RW/Scripts/Managers/UIManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance;
+
+    public Text sheepSavedText;
+    public Text sheepDroppedText;
+    public GameObject gameOverWindow;
+
     // Start is called before the first frame update
     void Start()
     {
